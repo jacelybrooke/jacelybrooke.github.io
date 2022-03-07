81a30e270e3f83de4f60ff8b1defcf2c7b55a7e6
jacelybrooke <jacelybrooke0317@gmail.com>
2022-03-07 19:04:35 +0000

    saving image filtering

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -21,12 +21,24 @@ var init = function (window) {
         
         // TODO 1 : Declare and initialize our variables
 
+// TODO 1: Declare our variables //
+var circle;			// variable to hold a single circle when creating circles / iterating
+var circles = [];	// variable to store all circles in one Array
 
         // TODO 2 : Create a function that draws a circle 
-        
+     function drawCircle(){
+         // Code to draw a circle
+        circle = draw.randomCircleInArea(canvas, true, true, '#999', 2);
+        physikz.addRandomVelocity(circle, canvas);
+        view.addChild(circle);
+        circles.push(circle);
+     }   
 
         // TODO 3 / 8 : Call the drawCircle() function 
-
+    for (var i=0; i<100; i++){
+         drawCircle();
+    }
+    
 
         ////////////////////////////////////////////////////////////
         ///////////////// PROGRAM LOGIC ////////////////////////////
@@ -39,7 +51,11 @@ var init = function (window) {
         */
         function update() {
             // TODO 4 : Update the circle's position //
-
+            for(var i=0;1<circles.length;i++){
+            physikz.updatePosition(circles[i]);
+            game.checkCirclePosition(circles[i]);
+            }
+       
             
             // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
            
@@ -62,7 +78,16 @@ var init = function (window) {
             }
             
             // TODO 7 : YOUR CODE STARTS HERE //////////////////////
+            if (circle.x < 0){
+                circle.x = canvas.width;
+            }
+            if (circle.y > canvas. height){
+                circle.y = 0;
             
+            }
+            if (circle.y < 0){
+                circle.y = canvas.height 
+            }
 
 
             // YOUR TODO 7 CODE ENDS HERE //////////////////////////
